e3a021434588671225fc2dbedac02d033e36a7f5
adityagaur2620-netizen <aditygaur2620@gmail.com>
2025-11-03 15:34:06 +0530

    assets

---
+++ src/assets/frontend_assets/assets.js
@@ -0,0 +1,253 @@
+import basket_icon from './basket_icon.png'
+import logo from './logo.png'
+import header_img from './header_img.png'
+import search_icon from './search_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.jpg'
+import menu_3 from './menu_3.jpg'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.jpg'
+import menu_6 from './menu_6.png'
+
+import food_1 from './food_1.jpg'
+import food_2 from './food_2.jpg'
+import food_3 from './food_3.jpg'
+import food_4 from './food_4.jpg'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.jpg'
+import food_8 from './food_8.jpg'
+import food_9 from './food_9.jpg'
+import food_10 from './food_10.jpg'
+import food_11 from './food_11.jpg'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.jpg'
+import food_16 from './food_16.jpg'
+import food_17 from './food_17.jpg'
+import food_18 from './food_18.jpg'
+import food_19 from './food_19.jpg'
+import food_20 from './food_20.jpg'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.jpg'
+
+import add_icon_white from './add_icon_white.png'
+import add_icon_green from './add_icon_green.png'
+import remove_icon_red from './remove_icon_red.png'
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+import linkedin_icon from './linkedin_icon.png'
+import facebook_icon from './facebook_icon.png'
+import twitter_icon from './twitter_icon.png'
+import cross_icon from './cross_icon.png'
+import selector_icon from './selector_icon.png'
+import rating_starts from './rating_starts.png'
+import profile_icon from './profile_icon.png'
+import bag_icon from './bag_icon.png'
+import logout_icon from './logout_icon.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets = {
+    logo,
+    basket_icon,
+    header_img,
+    search_icon,
+    rating_starts,
+    add_icon_green,
+    add_icon_white,
+    remove_icon_red,
+    app_store,
+    play_store,
+    linkedin_icon,
+    facebook_icon,
+    twitter_icon,
+    cross_icon,
+    selector_icon,
+    profile_icon,
+    logout_icon,
+    bag_icon,
+    parcel_icon
+}
+
+export const menu_list = [
+    {
+        menu_name: "Snacks",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Beverges",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Sandwich",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Chinese",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Pizza",
+        menu_image: menu_5
+    },
+    {
+        menu_name: "Special",
+        menu_image: menu_6
+    }
+    ]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Chips",
+        image: food_1,
+        price: 20,
+        category: "Snacks"
+    },
+    {
+        _id: "2",
+        name: "Samosa",
+        image: food_2,
+        price: 15,
+        category: "Snacks"
+    }, {
+        _id: "3",
+        name: "Burger",
+        image: food_3,
+        price: 35,
+        category: "Salad"
+    }, {
+        _id: "4",
+        name: "Hot Dog",
+        image: food_4,
+        price: 45,
+        category: "Snacks"
+    }, {
+        _id: "5",
+        name: "Maggi",
+        image: food_5,
+        price: 35,
+        category: "Snacks"
+    }, {
+        _id: "6",
+        name: "Peri Peri Fries",
+        image: food_6,
+        price: 50,
+        category: "Snacks"
+    }, {
+        _id: "7",
+        name: "Chai",
+        image: food_7,
+        price: 15,
+        category: "Beverges"
+    }, {
+        _id: "8",
+        name: "Cold drinks",
+        image: food_8,
+        price: 20,
+        category: "Beverges"
+    }, {
+        _id: "9",
+        name: "Lassi",
+        image: food_9,
+        price: 25,
+        category: "Beverges"
+    }, {
+        _id: "10",
+        name: "Coffee",
+        image: food_10,
+        price: 30,
+         category: "Beverges"
+        
+    }, {
+        _id: "11",
+        name: "Milkshake",
+        image: food_11,
+        price: 50,
+         category: "Beverges"
+    }, 
+    {
+        _id: "12",
+        name: "Vegan Sandwich",
+        image: food_12,
+        price: 35,
+        category: "Sandwich"
+    }, {
+        _id: "13",
+        name: "Grilled Sandwich",
+        image: food_13,
+        price: 40,
+        category: "Sandwich"
+    }, {
+        _id: "14",
+        name: "Bread Sandwich",
+        image: food_14,
+        price: 24,
+        category: "Sandwich"
+    }, {
+        _id: "15",
+        name: "Veg Noodles",
+        image: food_15,
+        price: 40,
+         category: "Noodles"
+    }, {
+        _id: "16",
+        name: "Hakka Noodles",
+        image: food_16,
+        price: 45,
+        category: "Noodles"
+    }, {
+        _id: "17",
+        name: "Fried Rice",
+        image: food_17,
+        price: 50,
+        category: "Noodles"
+    }, {
+        _id: "18",
+        name: "Cheese Pizza",
+        image: food_18,
+        price: 65,
+        category: "Pizza"
+    }, {
+        _id: "19",
+        name: "Tomato Pizza",
+        image: food_19,
+        price: 70,
+       category: "Pizza"
+    }, {
+        _id: "20",
+        name: "Corn Pizza",
+        image: food_20,
+        price: 75,
+        category: "Pizza"
+    }, {
+        _id: "21",
+        name: "Chole bature",
+        image: food_21,
+        price: 50,
+       category: "Special"
+    }, {
+        _id: "22",
+        name: "Rajma Chawal",
+        image: food_22,
+        price: 50,
+         category: "Special"
+    },
+    {
+        _id: "23",
+        name: "Chole Chawal",
+        image: food_23,
+        price:55,
+         category: "Special"
+    },
+    {
+        _id: "24",
+        name: "Puri-Sabji",
+        image: food_24,
+        price: 40,
+        category: "Special"
+    }
+]
