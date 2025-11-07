292ebd9b858ddeb17f94fb4854b8d9e78d042726
adityagaur2620-netizen <adityagaur2620@gmail.com>
2025-11-07 17:19:46 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://canteeno.netlify.app/";
+  const url = "https://food-delivery-backend-5b6g.onrender.com";
   const [token, setToken] = useState("");
   const [userType, setUserType] = useState("user"); // "user" or "admin"
   const [food_list, setFoodList] = useState(localFoodList); 
